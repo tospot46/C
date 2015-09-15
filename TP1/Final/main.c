d247c0c96a60f8c9e97274c34e95a090af7aecfe
bouju francois <bouju.francois@hotmail.fr>
2015-09-15 08:37:23 +0200

    TP1

--- TP1/Final/main.c
+++ TP1/Final/main.c
@@ -48,51 +48,48 @@ int main()
     float num, denom;
     float prod1, prod2;
 
-while(quit != 'y')
-{
-    printf("a) faire la somme des valeurs\n");
-    printf("b) faire des soustraction\n");
-    printf("c) faire la difference\n");
-    printf("d) faire le produit\n");
-    printf("Choisissez l'operation a effectuer : ");
+    	printf("a) faire la somme des valeurs\n");
+    	printf("b) faire des soustraction\n");
+    	printf("c) faire la difference\n");
+    	printf("d) faire le produit\n");
+    	printf("Choisissez l'operation a effectuer : ");
+    	scanf("%c", &choix);
 
-    scanf("%c", &choix);
+        switch (choix)
+    	{
+        	case 'a' :
+            	printf("Entre le premier chiffre : ");
+            	scanf("%f", &somme1);
+            	printf("entre le deuxieme chiffre : ");
+            	scanf("%f", &somme2);
+            	somme(somme1, somme2);
+            break;
 
-    switch (choix)
-    {
-        case 'a' :
-            printf("Entre le premier chiffre : ");
-            scanf("%f", &somme1);
-            printf("entre le deuxieme chiffre : ");
-            scanf("%f", &somme2);
-            somme(somme1, somme2);
-        break;
-        case 'b' :
-            printf("entre le premier chiffre : ");
-            scanf("%f", &sous1);
-            printf("entre le deuxieme chiffre : ");
-            scanf("%f", &sous2);
-            soustraction(sous1, sous2);
-        break;
-        case 'c' :
-            printf("entre le premier chiffre : ");
-            scanf("%f", &num);
-            printf("entre le deuxieme chiffre : ");
-            scanf("%f",&denom);
-            quotient(num, denom);
-        break;
-        case 'd' :
-            printf("entre le premier chiffre : ");
-            scanf("%f", &prod1);
-            printf("entre le deuxieme chiffre : ");
-            scanf("%f", &prod2);
-            produit(prod1, prod2);
-        break;
+        	case 'b' :
+            	printf("entre le premier chiffre : ");
+            	scanf("%f", &sous1);
+            	printf("entre le deuxieme chiffre : ");
+            	scanf("%f", &sous2);
+            	soustraction(sous1, sous2);
+            break;
 
-    }
-	
-	scanf("%c", &quit);
-	printf("\n\n");
-}
-    return 0;
+        	case 'c' :
+            	printf("entre le premier chiffre : ");
+            	scanf("%f", &num);
+            	printf("entre le deuxieme chiffre : ");
+            	scanf("%f",&denom);
+            	quotient(num, denom);
+            break;
+
+        	case 'd' :
+            	printf("entre le premier chiffre : ");
+            	scanf("%f", &prod1);
+            	printf("entre le deuxieme chiffre : ");
+            	scanf("%f", &prod2);
+            	produit(prod1, prod2);
+            break;
+
+
+       }
+    return (0);
 }
